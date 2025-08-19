8cce6ade69f62db6f640f7648492ce8c3a40cc71
hamandishemudiwa <hamandishemudiwa@gmail.com>
2025-08-19 15:44:10 +0200

    Add files via upload

---
+++ PROJECT FOR FIRST ASSIGNMENT.py
@@ -0,0 +1,17 @@
+# Constants are usually written in ALL_CAPS by convention
+PI = 3.14159   # Constant value for pi
+
+# Variables (values can change)
+radius = 5
+height = 20
+
+
+# Calculations using variables and constants
+circle_area = PI * radius ** 2
+cylinder_volume = PI * radius ** 2 * height
+
+# Output results using print()
+print("Radius:", radius)
+print("Height:", height)
+print("Circle Area:", circle_area)
+print("Cylinder Volume:", cylinder_volume)
